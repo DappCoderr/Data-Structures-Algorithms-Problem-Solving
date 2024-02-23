ecd00fb93f2a7102cc87a255327a0d5045fc3db9
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-02-23 13:13:40 +0530

    Update: sort color with 3 pointers, and anagam problem

--- ArrayHashing/8_sortColor.js
+++ ArrayHashing/8_sortColor.js
@@ -1,6 +1,42 @@
 // Problem: https://leetcode.com/problems/sort-colors/
 
-// Solved using Frequency mapping
+// Method 2
+var sortColors = function (nums) {
+  let zeros = 0;
+  let ones = 0;
+  let two = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      zeros++;
+    } else if (nums[i] === 1) {
+      ones++;
+    } else {
+      two++;
+    }
+  }
+  var k = 0;
+  while (k < nums.length) {
+    while (zeros > 0) {
+      nums[k] = 0;
+      zeros--;
+      k++;
+    }
+    while (ones > 0) {
+      nums[k] = 1;
+      ones--;
+      k++;
+    }
+    while (two > 0) {
+      nums[k] = 2;
+      two--;
+      k++;
+    }
+  }
+};
+
+sortColors([2, 0, 2, 1, 1, 0]);
+
+// Method 1 Solved using Frequency mapping (but not the optimal approach for all the test-cases)
 
 // var sortColors = function (nums) {
 //   const n = nums.length;
@@ -24,5 +60,3 @@
 //   }
 //   console.log(nums);
 // };
-
-// sortColors([1]);
